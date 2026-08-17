37098e7053178d3af1f5ce9d83c7eaa3c844f54b
Just Tantalum <xe.4f14.5d3.6s2@proton.me>
2026-08-17 19:14:05 +0000

    idk

---
+++ src/lib/config.ts
@@ -0,0 +1,8 @@
+export const settings = {
+  title: "Tantalum",
+  description: "Condemned to eternal thirst before an unreachable spring.",
+  icon: "/favicon.png",
+  keywords: "tantalo, tantalum, tantalumdev, 73, xe, portafolio desarrollador web, full stack developer, programador backend, desarrollo web moderno, proyectos JavaScript, Astro.js, Vercel, desarrollo frontend, programación en Javascript, aplicaciones web, desarrollador freelance, software developer portafolio, minimal dark portfolio",
+  url: "tantalum.vercel.app",
+  color: "#d32f2f"
+}
