9f4a0b6e7d05156709ecfcf20ded2992728936ef
Just Tantalum <xe.4f14.5d3.6s2@proton.me>
2026-08-18 00:12:44 +0000

    .

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,8 +1,16 @@
+
+
 export const settings = {
   title: "Tantalum",
   description: "Condemned to eternal thirst before an unreachable spring.",
   icon: "/favicon.png",
-  keywords: "tantalo, tantalum, tantalumdev, 73, xe, portafolio desarrollador web, full stack developer, programador backend, desarrollo web moderno, proyectos JavaScript, Astro.js, Vercel, desarrollo frontend, programación en Javascript, aplicaciones web, desarrollador freelance, software developer portafolio, minimal dark portfolio",
   url: "tantalum.vercel.app",
   color: "#d32f2f"
 }
+
+export const socials = [
+  {href: "", icon: "tabler:brand-github-filled"},
+  {href: "", icon: "tabler:brand-discord-filled"},
+  {href: "", icon: "tabler:brand-instagram-filled"},
+  {href: "", icon: "tabler:mail-filled"},
+]
